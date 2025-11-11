cbd9b287357e88287b9b650a3f1a01659a52f7af
JoaoRemondi <107283613+JoaoRemondi@users.noreply.github.com>
2025-11-11 12:22:00 -0300

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -298,21 +298,316 @@ const diagram = `{
 }`;
 
 const microPythonCode = `
-from machine import Pin
+from machine import Pin, ADC, PWM, Timer, SoftI2C
+from ssd1306 import SSD1306_I2C
+import neopixel
 import time
 
-led = Pin(11, Pin.OUT)
+#Matriz de Leds
+NUM_LEDS = 25       # 5x5 = 25 LEDs
+PIN_LED = 7        # pino conectado ao DIN (ajuste conforme seu circuito)
 
-def duty_cycle(ciclo_total, tempo_ligado):
-    led.value(1) 
-    time.sleep(tempo_ligado)
-    led.value(0)
-    time.sleep(ciclo_total - tempo_ligado)
+# Inicializa a matriz
+np = neopixel.NeoPixel(Pin(PIN_LED), NUM_LEDS)
 
+# Acende todos em vermelho
+for i in range(NUM_LEDS):
+    np[i] = (255, 0, 0)
+np.write()
+time.sleep(0.1)
+
+# Configuração dos botões
+button_a = Pin(5, Pin.IN, Pin.PULL_UP)
+button_b = Pin(6, Pin.IN, Pin.PULL_UP)
+button_c = Pin(10, Pin.IN, Pin.PULL_UP)
+
+#Define pinos de entrada da tela
+i2c = SoftI2C(scl=Pin(15), sda=Pin(14))
+#Define tamanho máximo da tela (128 pixels de largura e 64 pixels de altura)
+oled = SSD1306_I2C(128, 64, i2c)
+
+# Configuração dos pinos do joystick
+vrx = ADC(Pin(27)) # Eixo X
+vry = ADC(Pin(26)) # Eixo Y
+sw = Pin(22, Pin.PULL_DOWN) # Botão
+
+# Configuração dos pinos dos LEDs
+led_r = PWM(Pin(12, Pin.OUT))
+led_g = PWM(Pin(13, Pin.OUT))
+led_b = PWM(Pin(11, Pin.OUT))
+
+# Definição das frequências dos PWMs dos LEDs
+led_r.freq(50)
+led_g.freq(50)
+led_b.freq(50)
+
+def mostrar(texto):
+    oled.fill(0)
+    oled.text(texto, 0, 10)
+    oled.show()
+
+# Função para mapear o valor do joystick para o brilho do LED
+def map_value(value, in_min, in_max, out_min, out_max):
+  return int((value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+alto_falante = PWM(Pin(21))
+
+notas = {
+    'C4': 261,
+    'D4': 294,
+    'E4': 329,
+    'F4': 349,
+    'G4': 392,
+    'A4': 440,
+    'B4': 494,
+    'C5': 523,
+    'PAUSA': 0
+}
+
+musica = [
+    ('C4', 1), ('C4', 1), ('G4', 1), ('G4', 1), ('A4', 1), ('A4', 1), ('G4', 2),
+    ('F4', 1), ('F4', 1), ('E4', 1), ('E4', 1), ('D4', 1), ('D4', 1), ('C4', 2),
+]
+
+# Estado da música
+indice = 0
+tocando = False
+timer = Timer()
+
+def proximo_passo(t):
+    global indice, tocando
+
+    if not tocando:
+        return
+
+    if indice >= len(musica):
+        alto_falante.duty_u16(0)
+        indice = 0
+        tocando = False
+        return
+
+    nota, duracao = musica[indice]
+    freq = notas[nota]
+
+    if freq > 0:
+        alto_falante.freq(freq)
+        alto_falante.duty_u16(32768)
+    else:
+        alto_falante.duty_u16(0)
+
+    # Duracao base: 250ms por unidade
+    tempo_ms = 250 * duracao
+
+    # Agenda a próxima nota
+    timer.init(mode=Timer.ONE_SHOT, period=tempo_ms, callback=proximo_passo)
+
+    indice += 1
+
+def iniciar_musica():
+    global tocando, indice
+    if not tocando:
+        tocando = True
+        indice = 0
+        proximo_passo(None)  # inicia imediatamente
+
+#iniciar_musica()
+# Exemplo de uso
 while True:
-    # 100Hz → (1/100)s = 0,01s total de ciclo
-    # Vamos simular duty cycle mudando tempos manualmente
-    duty_cycle(0.01, 0.007)  # 70% de brilho
+
+    if button_a.value() == 0:
+        mostrar("Botao A pressionado")
+    elif button_b.value() == 0:
+        mostrar("Botao B pressionado")
+    elif button_c.value() == 0:
+        mostrar("Botao C pressionado")
+
+    x_val = vrx.read_u16()
+    y_val = vry.read_u16()
+
+    # Mapeia os valores do joystick para os valores de PWM dos LEDs
+    led_r.duty_u16(x_val)
+    led_g.duty_u16(y_val)
+    led_b.duty_u16(x_val // 2 + y_val // 2)
+
+    if sw.value() == 0: # Se o botão do joystick for pressionado
+        led_r.duty_u16(0)
+        led_g.duty_u16(0)
+        led_b.duty_u16(0)
+
+    # Pequena pausa para não sobrecarregar o processador.
+    time.sleep_ms(1)
+    iniciar_musica()  # dispara a música sem bloquear
+
+`;
+
+const ssd1306 = `
+#MicroPython SSD1306 OLED driver, I2C and SPI interfaces created by Adafruit
+
+import time
+import framebuf
+
+# register definitions
+SET_CONTRAST        = const(0x81)
+SET_ENTIRE_ON       = const(0xa4)
+SET_NORM_INV        = const(0xa6)
+SET_DISP            = const(0xae)
+SET_MEM_ADDR        = const(0x20)
+SET_COL_ADDR        = const(0x21)
+SET_PAGE_ADDR       = const(0x22)
+SET_DISP_START_LINE = const(0x40)
+SET_SEG_REMAP       = const(0xa0)
+SET_MUX_RATIO       = const(0xa8)
+SET_COM_OUT_DIR     = const(0xc0)
+SET_DISP_OFFSET     = const(0xd3)
+SET_COM_PIN_CFG     = const(0xda)
+SET_DISP_CLK_DIV    = const(0xd5)
+SET_PRECHARGE       = const(0xd9)
+SET_VCOM_DESEL      = const(0xdb)
+SET_CHARGE_PUMP     = const(0x8d)
+
+
+class SSD1306:
+    def __init__(self, width, height, external_vcc):
+        self.width = width
+        self.height = height
+        self.external_vcc = external_vcc
+        self.pages = self.height // 8
+        # Note the subclass must initialize self.framebuf to a framebuffer.
+        # This is necessary because the underlying data buffer is different
+        # between I2C and SPI implementations (I2C needs an extra byte).
+        self.poweron()
+        self.init_display()
+
+    def init_display(self):
+        for cmd in (
+            SET_DISP | 0x00, # off
+            # address setting
+            SET_MEM_ADDR, 0x00, # horizontal
+            # resolution and layout
+            SET_DISP_START_LINE | 0x00,
+            SET_SEG_REMAP | 0x01, # column addr 127 mapped to SEG0
+            SET_MUX_RATIO, self.height - 1,
+            SET_COM_OUT_DIR | 0x08, # scan from COM[N] to COM0
+            SET_DISP_OFFSET, 0x00,
+            SET_COM_PIN_CFG, 0x02 if self.height == 32 else 0x12,
+            # timing and driving scheme
+            SET_DISP_CLK_DIV, 0x80,
+            SET_PRECHARGE, 0x22 if self.external_vcc else 0xf1,
+            SET_VCOM_DESEL, 0x30, # 0.83*Vcc
+            # display
+            SET_CONTRAST, 0xff, # maximum
+            SET_ENTIRE_ON, # output follows RAM contents
+            SET_NORM_INV, # not inverted
+            # charge pump
+            SET_CHARGE_PUMP, 0x10 if self.external_vcc else 0x14,
+            SET_DISP | 0x01): # on
+            self.write_cmd(cmd)
+        self.fill(0)
+        self.show()
+
+    def poweroff(self):
+        self.write_cmd(SET_DISP | 0x00)
+
+    def contrast(self, contrast):
+        self.write_cmd(SET_CONTRAST)
+        self.write_cmd(contrast)
+
+    def invert(self, invert):
+        self.write_cmd(SET_NORM_INV | (invert & 1))
+
+    def show(self):
+        x0 = 0
+        x1 = self.width - 1
+        if self.width == 64:
+            # displays with width of 64 pixels are shifted by 32
+            x0 += 32
+            x1 += 32
+        self.write_cmd(SET_COL_ADDR)
+        self.write_cmd(x0)
+        self.write_cmd(x1)
+        self.write_cmd(SET_PAGE_ADDR)
+        self.write_cmd(0)
+        self.write_cmd(self.pages - 1)
+        self.write_framebuf()
+
+    def fill(self, col):
+        self.framebuf.fill(col)
+
+    def pixel(self, x, y, col):
+        self.framebuf.pixel(x, y, col)
+
+    def scroll(self, dx, dy):
+        self.framebuf.scroll(dx, dy)
+
+    def text(self, string, x, y, col=1):
+        self.framebuf.text(string, x, y, col)
+
+
+class SSD1306_I2C(SSD1306):
+    def __init__(self, width, height, i2c, addr=0x3c, external_vcc=False):
+        self.i2c = i2c
+        self.addr = addr
+        self.temp = bytearray(2)
+        # Add an extra byte to the data buffer to hold an I2C data/command byte
+        # to use hardware-compatible I2C transactions.  A memoryview of the
+        # buffer is used to mask this byte from the framebuffer operations
+        # (without a major memory hit as memoryview doesn't copy to a separate
+        # buffer).
+        self.buffer = bytearray(((height // 8) * width) + 1)
+        self.buffer[0] = 0x40  # Set first byte of data buffer to Co=0, D/C=1
+        self.framebuf = framebuf.FrameBuffer1(memoryview(self.buffer)[1:], width, height)
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.temp[0] = 0x80 # Co=1, D/C#=0
+        self.temp[1] = cmd
+        self.i2c.writeto(self.addr, self.temp)
+
+    def write_framebuf(self):
+        # Blast out the frame buffer using a single I2C transaction to support
+        # hardware I2C interfaces.
+        self.i2c.writeto(self.addr, self.buffer)
+
+    def poweron(self):
+        pass
+
+
+class SSD1306_SPI(SSD1306):
+    def __init__(self, width, height, spi, dc, res, cs, external_vcc=False):
+        self.rate = 10 * 1024 * 1024
+        dc.init(dc.OUT, value=0)
+        res.init(res.OUT, value=0)
+        cs.init(cs.OUT, value=1)
+        self.spi = spi
+        self.dc = dc
+        self.res = res
+        self.cs = cs
+        self.buffer = bytearray((height // 8) * width)
+        self.framebuf = framebuf.FrameBuffer1(self.buffer, width, height)
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        self.cs.high()
+        self.dc.low()
+        self.cs.low()
+        self.spi.write(bytearray([cmd]))
+        self.cs.high()
+
+    def write_framebuf(self):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        self.cs.high()
+        self.dc.high()
+        self.cs.low()
+        self.spi.write(self.buffer)
+        self.cs.high()
+
+    def poweron(self):
+        self.res.high()
+        time.sleep_ms(1)
+        self.res.low()
+        time.sleep_ms(10)
+        self.res.high()
 `;
 
 const outputText = document.getElementById('output-text');
@@ -323,6 +618,7 @@ window.addEventListener('message', (event) => {
   client.addEventListener('wokwi:connected', async (event) => {
     console.log('Wokwi client connected', event.detail);
     await client.serialMonitorListen();
+    await client.fileUpload('ssd1306.py', ssd1306);
     await client.fileUpload('main.py', microPythonCode);
     await client.fileUpload('diagram.json', diagram);
   });
